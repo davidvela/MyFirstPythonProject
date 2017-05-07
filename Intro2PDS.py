c803071465fb82e6b3031121c8769314f548c6d4
david <david.vela.tirado@gmail.com>
2017-05-07 03:03:06 +0200

    import packages!

--- Intro2PDS.py
+++ Intro2PDS.py
@@ -1,3 +1,6 @@
+# *************************************************
+#  INTRO TO PYTHON FOR DATA SCIENCE 
+# *************************************************
 
 # area variables (in square meters)
 hall = 11.25
@@ -19,3 +22,11 @@ print(str(house[1])+"="+str(house[-4]))
 # array manipulation - slice inclusive:exclusive - del(array[2]) 
 
 # funcitons: max, len, sorted, complex, 
+# methods: capitalize, replace, bit_length, conjugate, index, count, append, reverse, remove
+HallwayIndex = house.index("hallway")
+
+# Numpy:        For efficient work in arrays
+# Matplotlib:   Data visualization
+# Scikit-learn: Machine Learning  
+
+# install package: pip3 install // pip.readthedocs.org ! 

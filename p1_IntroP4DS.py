bc87c16c2e9c04d61be0a51fef354ea98b28af51
david <david.vela.tirado@gmail.com>
2017-05-07 22:14:27 +0200

    Python DataCamp! level 2

--- p1_IntroP4DS.py
+++ p1_IntroP4DS.py
@@ -21,7 +21,7 @@ print(type(house)); print( house ); print(house[0]);
 print(str(house[1])+"="+str(house[-4]))
 # array manipulation - slice inclusive:exclusive - del(array[2]) 
 
-# funcitons: max, len, sorted, complex, 
+# funcitons: max, len, sorted, complex,  // help(max) - documentation
 # methods: capitalize, replace, bit_length, conjugate, index, count, append, reverse, remove
 HallwayIndex = house.index(["hallway",hall])
 
@@ -31,8 +31,16 @@ HallwayIndex = house.index(["hallway",hall])
 
 # install package: pip3 install // pip.readthedocs.org ! import as // from .. import .. 
 # List - slow , operations/calculations for all data collecitons 
-# Numpy - fast and possible  = numeric python
+# Numpy - fast and possible  = numeric python - arrays of single types!
+
+# *************************************************
+#   NUMPY - way faster 
+# *************************************************
 import numpy as np 
 np_house = np.array(house)
 # np_house_2 = np_house * 2
 # print(np_house_2)
+
+# np_boolean = np_array < 21 : print(np_array[np_boolean])
+# 2D numpy array: np.array( [1,2],[3,4]).shape = (2,2)  2Daray[:2, 1]
+# FUNCTIONS: np.mean, np.median, corrcoef, std - standard deviation, sum(), sort(), np.round, np.random.normal 
---
+++ p2_IntermP4DS.py
@@ -0,0 +1,11 @@
+
+# *************************************************
+#  INTERMEDIATE PYTHON FOR DATA SCIENCE 
+# *************************************************
+
+# 1 NATPLOTLIB! 
+# 2 DICTIONARIES & PANDAS  
+# 3 LOGIC, CONTORL FLOW  AND FILTER
+# 4 LOOPS 
+# 5 CASE STUDY: HACKER STATISTICS
+

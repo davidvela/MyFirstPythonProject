c719fe4b4dbb5180714f23795021dee63ea19d2c
david <david.vela.tirado@gmail.com>
2017-05-09 07:46:47 +0200

    matplotlib

--- p2_IntermP4DS.py
+++ p2_IntermP4DS.py
@@ -10,4 +10,11 @@
 # 5 CASE STUDY: HACKER STATISTICS
 
 import matplotlib.pyplot as plt 
+import numpy as np 
 
+year = [2010,2011,2012] # x
+plot = [2010,2011,2012] # y
+
+# Print the last item from year and pop
+print(year[-1]); print(pop[-1])
+plt.plot(year,pop); plt.show()

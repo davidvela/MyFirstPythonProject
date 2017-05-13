7a7941604fc72e71ea9927b306f3cb3d45d66bb2
david <david.vela.tirado@gmail.com>
2017-05-13 02:51:11 +0200

    visualization matplot - hist and scatter

--- p2_IntermP4DS.py
+++ p2_IntermP4DS.py
@@ -1,12 +1,12 @@
 
 # *************************************************
-#  INTERMEDIATE PYTHON FOR DATA SCIENCE 
+# INTERMEDIATE PYTHON FOR DATA SCIENCE 
 # *************************************************
 
 # 1 NATPLOTLIB! 
-# 2 DICTIONARIES & PANDAS  
-# 3 LOGIC, CONTORL FLOW  AND FILTER
-# 4 LOOPS 
+# 2 DICTIONARIES AND PANDAS.  
+# 3 LOGIC, CONTORL FLOW  AND FILTER.
+# 4 LOOPS. 
 # 5 CASE STUDY: HACKER STATISTICS
 
 import matplotlib.pyplot as plt 
@@ -17,4 +17,9 @@ plot = [10,20,30] # y
 
 # Print the last item from year and pop
 print(year[-1]); print(plot[-1])
-plt.plot(year,plot); plt.show()
+# plt.plot(year,plot); 
+plt.scatter(year, plot)
+# Put the x-axis on a logarithmic scale - plt.xscale('log')
+plt.show()
+
+# documentation - help(plt.hist)

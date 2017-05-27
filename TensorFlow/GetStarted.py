d0cdeec48fa3c74b2981875b7a9a0f1fe413ee8a
david <david.vela.tirado@gmail.com>
2017-05-27 02:17:53 +0200

    last learning before holidays =P

--- TensorFlow/GetStarted.py
+++ TensorFlow/GetStarted.py
@@ -1,4 +1,17 @@
 import tensorflow as tf
 hello = tf.constant('Hello, TenshorFlow!')
+a = tf.constant([2])
+b = tf.constant([3])
+c = tf.add(a, b)
+
 sess = tf.Session()
-print(sess.run(hello))
+
+
+
+# print(sess.run(hello))
+print(sess.run(c))
+sess.close()
+
+with tf.Session() as sess:
+    result = sess.run(c)
+    print(result)

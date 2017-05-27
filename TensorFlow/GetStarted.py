557276cb35703c74ba490d05201a4b2eaf2ed0f4
david <david.vela.tirado@gmail.com>
2017-05-27 12:56:29 +0200

    learning tensors and matrix

--- TensorFlow/GetStarted.py
+++ TensorFlow/GetStarted.py
@@ -14,3 +14,24 @@ with tf.Session() as sess:
 
 # Multidimensional Arrays
 Scalar = tf.constant([2])
+Vector = tf.constant([5,6,2])
+Matrix = tf.constant([[1,2,3],[2,3,4],[4,5,6]])
+Tensor = tf.constant([  [[1,2,3],[2,3,4],[4,5,6]], [[1,2,3],[2,3,4],[4,5,6]] ] )
+
+Matrix_one = tf.constant([ [1,1],[2,2] ])
+Matrix_two = tf.constant([ [2,3],[2,3] ])
+# Regular Matrix product : - * | = [4, 6], [8, 12] 
+first_operation = tf.matmul(Matrix_one, Matrix_two)
+
+with tf.Session() as sess:
+    result = sess.run(Tensor)
+    print( "Tensor (3x3x3 entries): \n" ,  result )
+
+    result = sess.run(first_operation)
+    print( " \n Multiplication:  \n" ,  result )
+
+
+
+
+# Placeholder are like variables -> you have to define the type: 
+# placeholder = tf.placeholder()

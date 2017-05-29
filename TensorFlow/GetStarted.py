9510853f16b5e561c501ba37bba28a5c0b59d37e
david <david.vela.tirado@gmail.com>
2017-05-29 23:20:30 +0200

    Linear regression representation

--- TensorFlow/GetStarted.py
+++ TensorFlow/GetStarted.py
@@ -37,7 +37,7 @@ state   = tf.Variable(0)
 one     = tf.constant(1)
 new     = tf.add(state, one)
 update  = tf.assign(state, new)
-init_op = tf.initialize_all_variables()
+init_op = tf.global_variables_initializer()
 
 
 with tf.Session() as session:
@@ -85,7 +85,7 @@ Y = a*X + b
 plt.plot(X,Y)
 plt.ylabel('Dependent Variable')
 plt.xlabel('Independent Variable')
-# plt.show()
+#plt.show()
 
 x_data = np.random.rand(100).astype(np.float)
 # model: Y = 3X + 2
@@ -102,8 +102,35 @@ loss        = tf.reduce_mean(tf.square(y - y_data))
 optimizer   = tf.train.GradientDescentOptimizer(0.5)
 train       = optimizer.minimize(loss)
 
-init        = tf.initialize_all_variables( )
+init        = tf.global_variables_initializer( )
 sess        = tf.Session()
 sess.run(init)
+
+# train the model  
 train_data = []
-#for step in range(100)
+for step in range(100):
+    evals = sess.run([train, a, b])[1:]
+    if step % 5 == 0:
+        print(step, evals)
+        train_data.append(evals)
+
+# Display train data 
+converter = plt.colors
+cr, cg, cb = (1.0, 1.0, 0.0)  
+    # green = 110 / 
+for f in train_data:
+    cb += 1.0 / len(train_data)
+    cg -= 1.0 / len(train_data)
+    if cb > 1.0: cb = 1.0
+    if cg < 0.0: cg = 0.0
+    [a,b] = f
+    f_y = np.vectorize(lambda x: a*x + b)(x_data)
+    line = plt.plot(x_data, f_y)
+    plt.setp(line, color=(cr,cg,cb))
+
+plt.plot(x_data, y_data, 'ro')
+
+green_line = mpatches.Patch(color = 'red', label = 'Data Points')
+plt.legend(handles=[green_line])
+# plt.show()
+

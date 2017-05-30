b1a6f71de2fcae29641deef94fc130c0d6671ae1
david <david.vela.tirado@gmail.com>
2017-05-30 09:48:30 +0200

    starting module 2: Convolutional networks

---
+++ TensorFlow/BDU_M2.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.patches as mpatches 
+import matplotlib.pyplot as plt 
+
+# TensorFlow - lesson 1 Big Data University
+#----------------------------------------------
+#       Module 2 - convolutional networks 
+#  original goal of machine learning: 
+#   "Move humananity closer to the undreachable General AI"

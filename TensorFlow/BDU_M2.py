22800d925ce89357d2011b6124ba81a7815925a7
david <david.vela.tirado@gmail.com>
2017-05-30 20:11:22 +0200

    intro to CNN

--- TensorFlow/BDU_M2.py
+++ TensorFlow/BDU_M2.py
@@ -8,3 +8,25 @@ import matplotlib.pyplot as plt
 #       Module 2 - convolutional networks 
 #  original goal of machine learning: 
 #   "Move humananity closer to the undreachable General AI"
+
+# fully connected layer 
+# - each neuron connected to every neuron in previuos layer
+# - each connection has it's own weight 
+# - no assumptions about the feautres in the input data 
+# - type of layers are also very expensive in terms of memory and computation
+#   
+# convolutional layer 
+# - each neuron is only connected to a few nearby local neurons in previosu layer
+# - same weights is used for every neuron 
+# - this connection patterns only makes sense for cases where:
+#   - data can be interpreted as spatial with the fatures to be extracetd being spatially local 
+#       (e.g. a window consists of a set of pixels around an area, not spread all across the image)
+#   - equally likely to occur at any input position (hence some weights at all positions)
+#       (e.g. window can occur anywhere in the image and the image can be rotated or scaled)
+#  
+#
+# FEATURE LEARNING  
+#   Feature enginering is the process of extracting useful patterns from input data
+#   that will help prediction model to understand better the real nature of the problem.  
+#  CNN good for finding patterns - Feature Engineering 
+#  Combining simple features to create bigger pictures s

c0f0301a1079d412f03a949fce9559abeb2182fc
david <david.vela.tirado@gmail.com>
2017-06-05 20:17:02 +0200

    testing

--- TensorFlow/SR_Deploy.py
+++ TensorFlow/SR_Deploy.py
@@ -1,1 +1,8 @@
-# tensorflow server -> deploy models 
+# tensorflow Serving library -> deploy models 
+# you can have one model or multiple models
+# 1. load model 2. serve the model  
+# gRPC Client - RPC protocol on top of HTTP (GET POST)- vs REST's 
+
+# different version of model: servables, loaders, sources, managers
+
+

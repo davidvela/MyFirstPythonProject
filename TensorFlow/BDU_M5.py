a3f35a8dce0804dcaa76f313555159abad66499d
david <david.vela.tirado@gmail.com>
2017-06-06 21:13:20 +0200

    finish theory BDU DL TF

--- TensorFlow/BDU_M5.py
+++ TensorFlow/BDU_M5.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 
 # TensorFlow - lesson 1 Big Data University
 #----------------------------------------------
-# MODULE 3 - Recurrent Neural Networks (RNN)
-#   Sequential limitaion of the traditional approach 
-#   RNN limitations regarding states and gradient explotions 
-#   RNN solution LSTM - Long Short Term Memory 
-#   ex. Twitter sentiment analysis 
+# MODULE 5 - AUTOENCODERS 
+#   Autoencoders and Deep Belief Networks
+
 

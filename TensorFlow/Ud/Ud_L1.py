40aa6f9f0b01e2d85deabe06405f52b8b2708a89
David Vela Tirado <david.vela.tirado@gmail.com>
2017-06-10 08:48:47 +0200

    softmax function in tf

--- TensorFlow/Ud/Ud_L1.py
+++ TensorFlow/Ud/Ud_L1.py
@@ -1,6 +1,7 @@
 # Udacity Lesson 1: From ML to DL 
 
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
 # 1.10 - Softmax
@@ -11,8 +12,16 @@ scores2 = np.vstack([x, np.ones_like(x), 0.2*np.ones_like(x)] )
 def softmax(x):
  return np.exp(x) / np.sum(np.exp(x), axis = 0)
 
+sess          = tf.Session()
+softmaxTensor = tf.nn.softmax(scores)
 print( softmax(scores) )
+print( softmaxTensor  )
+print(sess.run(softmaxTensor))
 
 plt.plot(x, softmax(scores2).T, linewidth = 2 )
+plt.ylabel('Probabillities')
+plt.xlabel('x values')
+plt.title("Softmax function")
+
 plt.show()
 

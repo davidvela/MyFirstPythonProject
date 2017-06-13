05f5cd90fab071ef9a5e79ede5fc07c88f2ad82f
David Vela Tirado <david.vela.tirado@gmail.com>
2017-06-13 22:39:39 +0200

    Standford Univ. course

---
+++ TensorFlow/UnST/Intro.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.patches as mpatches 
+import matplotlib.pyplot as plt 
+
+# TensorFlow - lesson 1 Big Data University
+#----------------------------------------------
+
+hello = tf.constant('Hello, TenshorFlow!')
+a = tf.constant([2])
+b = tf.constant([3])
+c = tf.add(a, b)
+sess = tf.Session()
+with tf.Session() as sess:
+    result = sess.run(c)
+    print(result)
+

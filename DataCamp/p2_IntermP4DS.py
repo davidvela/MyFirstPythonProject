b01373a02ce1da1323b411015bf293af37f7e5c4
DAVID VELA TIRADO <david.vela.tirado@gmail.com>
2017-06-25 01:09:07 +0200

    test pandas library

--- DataCamp/p2_IntermP4DS.py
+++ DataCamp/p2_IntermP4DS.py
@@ -15,6 +15,7 @@
 
 import matplotlib.pyplot as plt 
 import numpy as np 
+import os
 
 year = [2010,2011,2012] # xinstall 
 plot = [10,20,30] # y
@@ -39,3 +40,7 @@ plt.show()
 
 # 2 DICTIONARIES AND PANDAS.  
 # pandas - get data from excel, csv, read sql 
+if os.path.exists("iris_test.csv"):
+    test = pd.read_csv("iris_test.csv") #index_col = 0
+    print(test) #Columns = variables; rows = observations!
+

db176a366128bce10455be66765d39675d408a2e
david <david.vela.tirado@gmail.com>
2017-06-29 01:12:42 +0200

    unsupervised learning theory

--- DataCamp/pUL_UnsupervisedLearning.py
+++ DataCamp/pUL_UnsupervisedLearning.py
@@ -10,7 +10,32 @@
 #   k-means sklearn 
 from sklearn.cluster import KMeans
 
-sample = [  [5., 3.3, 1.4, 0.2], 
+samples = [  [5., 3.3, 1.4, 0.2], 
             [4., 3.3, 1.4, 0.2],]
-            
+
 model = KMeans(n_clusters=3)
+model.fit(samples)
+labels = model.predict(samples)
+print(labels)
+
+# visualize in scatterplot
+import matplotlib.pyplot as plt
+xs = samples[:,0]
+ys = samples[:,1]
+plt.scatter(xs,ys, c=labels, alpha=0.5)
+
+# Assign the cluster centers: centroids
+centroids = model.cluster_centers_
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1] # Diamon market
+plt.scatter(centroids_x,centroids_y, marker='D', s=50)
+plt.show()
+
+#Cross tabulations - when cluster mix different species 
+
+# inertia measures 
+
+
+#standard scaler - normalization 0-1 - 
+# others: MaxAbsScaler and Normalizer
+# 2 steps - standardscaler and kmeans => combine two steps with a pipeline 

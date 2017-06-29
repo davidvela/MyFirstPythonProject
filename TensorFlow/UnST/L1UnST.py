ea8016a3e5fbe293912f25a866c3448579cf441a
David Vela Tirado <david.vela.tirado@gmail.com>
2017-06-30 00:32:32 +0200

    lesson 3 std. un

---
+++ TensorFlow/UnST/L1UnST.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+#import matplotlib.patches as mpatches 
+#import matplotlib.pyplot as plt 
+
+# TensorFlow - lesson 1 and 2 Standfor University
+# # visualize graphs in tf: writer ​=​ tf​.​summary​.​FileWriter​(​'./graphs'​,​ sess​.​graph)
+# $ python ​[​yourprogram​.​py​]
+# $ tensorboard ​--​logdir​=​"./graphs
+#----------------------------------------------
+
+hello = tf.constant('Hello, TensorFlow!')
+a = tf.constant([2], name = "a" )
+b = tf.constant([3], name = "b")
+c = tf.add(a, b, name = "add")
+sess = tf.Session()
+with tf.Session() as sess:
+    writer ​=​ tf​.​summary​.​FileWriter​(​'./graphs'​,​ sess​.​graph)
+    result = sess.run(c)
+    print(result)
+
+# create tensors: 
+# tf.constant([2,2]), 
+# f​.​zeros​([​2​,​ ​3​],​ tf​.​int32​)​ ​==>​ ​[[​0​,​ ​0​,​ ​0​],​ ​[​0​,​ ​0​,​ ​0​]]   // ones
+# tf​.​zeros_like​(​input_tensor​)​  //ones_like
+# tf.fill(dims, value)
+# ..
+
---
+++ TensorFlow/UnST/L3UnST.py
@@ -0,0 +1,27 @@
+# Linear Regr. - lesson 3 TF Standfor University
+# ph1 - assemble a graph
+# ph2 - use session to execute operations in a graph 
+#. 
+# Feed values into placeholders or variables 
+# .
+# 03_linear_regr. fire_thef.xls x = fires y = theft.
+# want: Predict thefts from fires 
+# model = W*x + b.   (Y-YP)^2 
+# . 
+#----------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import xlrd
+
+DATA_FILE = 'data/fire_theft.xls'
+
+# Phase 1: Assemble the graph
+#------------------------------------------------
+# Step 1: read in data from the .xls file
+book = xlrd.open_workbook(DATA_FILE, encoding_override='utf-8')
+sheet = book.sheet_by_index(0)
+data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
+n_samples = sheet.nrows - 1
+
+print(n_samples)

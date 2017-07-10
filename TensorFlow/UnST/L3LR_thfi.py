a06685ce49681d00d91648afc3423bccad4ca1bb
david <david.vela.tirado@gmail.com>
2017-07-11 00:24:04 +0200

    testing visualization + linear regression

--- TensorFlow/UnST/L3LR_thfi.py
+++ TensorFlow/UnST/L3LR_thfi.py
@@ -28,8 +28,16 @@ DATA_FILE = 'data/fire_theft.xls'
 # Step 1: read in data from the .xls file
 book = xlrd.open_workbook(DATA_FILE, encoding_override='utf-8')
 sheet = book.sheet_by_index(0)
-data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
+# data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
+# generate test data: y = 3x + b
 n_samples = sheet.nrows - 1
+
+X_input = np.linspace(- 1 , 1 , 100)
+Y_input = X_input * 3 + np.random.randn( X_input.shape[ 0 ]) * 0.5
+data.T[0] = X_input
+data.T[1] = Y_input
+n_samples = 99
+
 # Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
 X = tf.placeholder(tf.float32, name='X')
 Y = tf.placeholder(tf.float32, name='Y')
@@ -52,7 +60,8 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss
 
 # Phase 1: Train our model - 
 #------------------------------------------------
-with tf.Session() as sess:
+#with tf.Session() as sess:
+with tf.InteractiveSession() as sess: # Test interactive session.
 	# Step 7: initialize the necessary variables, in this case, w and b
 	sess.run(tf.global_variables_initializer()) 
 	writer = tf.summary.FileWriter('./my_graph/03/linear_reg', sess.graph)
@@ -77,6 +86,7 @@ print("value and error: ", w_value, b_value)
 
 # plot the results
 X, Y = data.T[0], data.T[1]
+
 plt.plot(X, Y, 'bo', label='Real data')
 plt.plot(X, X * w_value + b_value, 'r', label='Predicted data')
 plt.legend()

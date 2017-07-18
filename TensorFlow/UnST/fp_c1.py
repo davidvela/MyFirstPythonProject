73695e5abd5e1d237a622942b8bed86186e27f2d
david <david.vela.tirado@gmail.com>
2017-07-18 09:58:37 +0200

    NN Regresion and Classification

--- TensorFlow/UnST/fp_c1.py
+++ TensorFlow/UnST/fp_c1.py
@@ -1,11 +1,10 @@
 # Classification Model [4885 rows x 1221 columns]
-# tensorboard --logdir=.\my_graph\0F\
+# tensorboard --logdir=.\my_graph\0FCR\
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 from fp_dr import get_data, next_batch
-
+import sys
 import os
 import urllib.request as urllib7m   
 import pandas as pd
@@ -19,80 +18,87 @@ ytt         = []
 xtp1        = []  
 ytp1        = []
 
-#Directories
 LOGDIR      = "./my_graph/0FCR/"
 TRAI_DS     = "../../knime-workspace/Data/FP/TFFRFL_ALSNT.csv"
 TEST_DS     = "../../knime-workspace/Data/FP/TFFRFL_ALSNE.csv"
-model_path = LOGDIR + "model.ckpt"
-
+model_path  = LOGDIR + "model.ckpt"
 
 # Parameters
-dv = 2   
 learning_rate = 0.001
 batch_size = 128
 training_iters = 1000 #200000
 display_step = training_iters*0.1 #10%
 record_step  = training_iters*0.005
 # Network Parameters
-n_hidden_1 = 256 # 1st layer number of features
-n_hidden_2 = 256 # 2nd layer number of features
-n_input = 1221 # MNIST data input (img shape: 28*28)
-n_classes = 4 # MNIST total classes (0-9 digits)
+n_hidden_1  = 256   # 1st layer number of features
+n_hidden_2  = 256   # 2nd layer number of features
+n_input     = 1221  # data input (img shape: 28*28)
+n_classes   = 4     # total classes 
 
 # Model variables
 x = tf.placeholder(tf.float32,   shape=[None, n_input],   name="x")
 y = tf.placeholder(tf.int16,     shape=[None, n_classes], name="cat")
 weights = {
-    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1]),    name="Weights_1"),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name="Weights_2"),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]), name="Weights_out"),
 }
 biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_classes]))
+    'b1': tf.Variable(tf.random_normal([n_hidden_1]), name="Bias_1"),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2]), name="Bias_2"),
+    'out': tf.Variable(tf.random_normal([n_classes]), name="Bias_out"),
 }
-xt, yt      = get_data(TRAI_DS)
-xtt, ytt    = get_data(TEST_DS)
+xt, yt      = get_data(TRAI_DS, 0)
+xtt, ytt    = get_data(TEST_DS, 0)
 # String for the logs
-def make_hparam_string(learning_rate, use_two_fc, use_two_conv):
-    #conv_param = "conv=2" if use_two_conv else "conv=1"
-    fc_param = "fc=2" if use_two_fc else "fc=1"
-    return "lr_%.0E,%s" % (learning_rate, fc_param) 
+def make_hparam_string(learning_rate, no_fc):
+    return "lr_%.0E,fc=%d" % (learning_rate, no_fc) 
 
 # Create model
 def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
+    with tf.name_scope("fc_1"):
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        layer_1 = tf.nn.relu(layer_1)
     # Hidden layer with RELU activation
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
+    with tf.name_scope("fc_2"):
+        layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        layer_2 = tf.nn.relu(layer_2)
     # Output layer with linear activation
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    with tf.name_scope("fc_output"):
+        out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
     return out_layer
 
 # - declaration of model and global attributes 
 pred = multilayer_perceptron(x, weights, biases)
-correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
+with tf.name_scope("accuracy"):
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar("accuracy", accuracy)
+with tf.name_scope("xent"):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    tf.summary.scalar("xent", cost)
+with tf.name_scope("train"):
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+summ = tf.summary.merge_all()
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
 #
-def train_model(): 
+def train_model(hparam): 
     # Running first session
     print("Starting 1st session...")
     with tf.Session() as sess:
         # Initialize variables
         sess.run(init)
+        writer = tf.summary.FileWriter(LOGDIR + hparam)
+        writer.add_graph(sess.graph)
         for i in range(training_iters):  
             xtb, ytb = next_batch(batch_size, xt, yt)
             if i % record_step == 0:
-                #[train_accuracy, s] = sess.run([accuracy, summ], feed_dict={x: xt, y: yt }) 
-                [train_accuracy] = sess.run([accuracy], feed_dict={x: xtb, y: ytb }) 
+                [train_accuracy, s] = sess.run([accuracy, summ], feed_dict={x: xtb, y: ytb }) 
+                writer.add_summary(s, i)
             if i % display_step == 0:
                 print("step %d, training accracy %g" %(i, train_accuracy))
             sess.run(optimizer, feed_dict={x: xtb, y: ytb})
@@ -116,15 +122,23 @@ def test_model():
         print("Predicted value:", sess.run(pred, feed_dict={x: xtp1}) ) 
 #
 #
-def main():
+def main(dv):
     # Construct model
     xtp1.append(xtt[1]);    ytp1.append(ytt[1])
+    hparam = make_hparam_string(learning_rate, 3)
 
     if dv == 1:
-        train_model()
+        train_model(hparam)
     elif dv == 2: 
         test_model()
     print('Run `tensorboard --logdir=%s` to see the results.' % LOGDIR)
 
 if __name__ == "__main__":
-    main()    
+    if len(sys.argv) > 1:
+        dv = int(sys.argv[1])
+        if dv > 0 and dv < 3 :
+            main(dv)    
+        else: print ("please type 1 for training and 2 for testing")
+    else: print ("please type 1 for training and 2 for testing")
+
+#end 
--- TensorFlow/UnST/fp_dr.py
+++ TensorFlow/UnST/fp_dr.py
@@ -20,18 +20,35 @@ def classif(df):
     elif( df >= 40 and df < 60 ): return [0,0,1,0]
     elif( df >= 60 and df < 90 ): return [0,1,0,0] 
     elif( df >= 90 ): return [1,0,0,0]
+def regress(df):
+    return [df]
 
-def get_data(path):
+def get_data(path, type):
     dst =  pd.read_csv( tf.gfile.Open(path), sep=None, skipinitialspace=True,  engine="python")
     dst = dst.fillna(0)
 
-    dst.insert(2, 'FP_C', dst['FP'].map(lambda x: classif(x)))
-
-
-    cat  = dst.loc[:,'FP_C']
-    dat  = dst.iloc[:, 3:]
-    # catL = list(cat.values.flatten())
-    catL = cat.as_matrix().tolist()
-    datL = dat.as_matrix().tolist()
-        
+    if type == 0:       # Classification in categories
+        dst.insert(2, 'FP_C', dst['FP'].map(lambda x: classif(x)))
+        cat  = dst.loc[:,'FP_C']
+        dat  = dst.iloc[:, 3:]
+        # catL = list(cat.values.flatten())
+        catL = cat.as_matrix().tolist()
+        datL = dat.as_matrix().tolist()
+    elif type == 1:     # Regression
+        dst.insert(2, 'FP_R', dst['FP'].map(lambda x: regress(x)))
+        cat  = dst.loc[:,'FP_R']
+        dat  = dst.iloc[:, 3:]
+        #print(cat)
+        catL = cat.as_matrix().tolist()
+        datL = dat.as_matrix().tolist()
+    
     return datL, catL
+
+# test logic: 
+# TRAI_DS     = "../../knime-workspace/Data/FP/TFFRFL_ALSNT.csv"
+# xt, yt      = get_data(TRAI_DS, 1)
+# print(yt)
+# for i in range(2):  
+#   xtb, ytb = next_batch(10, xt, yt)
+#   print(ytb)
+#   print("--new--")

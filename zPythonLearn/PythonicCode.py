020b972a135756cfc582849e28119a1f8c3d20eb
david <david.vela.tirado@gmail.com>
2017-07-20 01:02:12 +0200

    pythonic tutorial 55m

--- zPythonLearn/PythonicCode.py
+++ zPythonLearn/PythonicCode.py
@@ -84,14 +84,60 @@ def tupples():
 #---------------------------------------------------------------
 # 4. ignoring val, exceptions 
 # ---------------------------------------------------------------
+class DownloadService:
+    def check_download_url(self):
+        pass
+
+    def check_access_allowed(self):
+        pass
+
+    def check_network(self):
+        pass
+
+    def download_file(self):
+        pass
+
+def save_file(file_name, data):
+    pass
+
+# LBYL look before you leap
 def ign():
-    return 
+    s = DownloadService()
+
+    if not s.check_download_url():
+        print("Cannot download, invalid url")
+    if not s.check_access_allowed():
+        print("Cannot download, permission denied")
+    if not s.check_network():
+        print("Cannot download, not connected")
+
+    data = s.download_file()
+    save_file('latest.png', data)
+    print('Successfully downloaded latest.png')
+
+    # EAFF Easier to ask for forgivenes than permission 
+    try:
+        data = s.download_file()
+        save_file('latest.png', data)
+        print('Successfully downloaded latest.png')
+    except SocketError as se:
+        print("Sorry no network: {}".format(se))
+    except Exception as x:
+        print("Sorry didn't work: {}".format(x))
+
 
 #---------------------------------------------------------------
 # 5. LAMBDA 
 # ---------------------------------------------------------------
 def lambdaa():
 
+
+
+    return 
+
+
+
 if __name__ == "__main__":
     # strings()
-    merg_dict()
+    ign()
+    #min 55

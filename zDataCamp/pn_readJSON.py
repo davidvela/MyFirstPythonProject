4471186238f6bffd55d001502ab462341a0bfa05
david <david.vela.tirado@gmail.com>
2017-07-19 10:14:36 +0200

    read odata ws

--- zDataCamp/pn_readJSON.py
+++ zDataCamp/pn_readJSON.py
@@ -1,15 +1,14 @@
 import json
 # import urllib2
-# from six.moves import urllib
 import requests
 url_oData_people = "http://services.odata.org/TripPinRESTierService/(S(pk4yy1pao5a2nngmm2ecx0hy))/People"
 
 
 response = requests.get( url_oData_people )
 people   = response.json()
-
-# people = json.load(  urllib.request.urlretrieve(url_oData_people)  )
 # people = json.load(urllib2.urlopen(url_oData_people))
-
 print(people)
+
+# CONVERT JOSN into object -> Pandas or dictionary array.7
+
 

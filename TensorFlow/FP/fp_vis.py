dd75b4bf529d16b0b9a3a1f16344fe57f602b3dd
david <david.vela.tirado@gmail.com>
2017-07-27 16:56:16 +0200

    visualization of FRAL

--- TensorFlow/FP/fp_vis.py
+++ TensorFlow/FP/fp_vis.py
@@ -1,8 +1,32 @@
 import pandas as pd 
-import tensorflow as tf 
 import numpy as np 
+import tensorflow as tf 
+import matplotlib.pyplot as plt
 
-dst =  pd.read_csv( tf.gfile.Open(path), sep=None, skipinitialspace=True,  engine="python")
-#C1 - M; C2 FP; C3; FF
+# 1- Ger data: 
+
+path = "../../knime-workspace/Data/FP/TFFRAL_ALSNAC.csv" 
+dst  =  pd.read_csv( tf.gfile.Open(path), sep=None, skipinitialspace=True,  engine="python")
+#C1 M; C2 FP; C3 FN; C4 SFM; C5 AP
+Y  = dst.loc[:,'FP'].as_matrix().tolist()
+X  = dst.loc[:, 'M'].as_matrix().tolist()
+
+# d = {ni: indi for indi, ni in enumerate(set(names))}
+# numbers = [d[ni] for ni in names]
+
+# 2 - Create plot. 
+a = 1
+if a == 0: 
+    plt.hist(Y)
+else: 
+    plt.xlabel('M')
+    plt.ylabel('FP')
+    plt.grid(True)
 
 
+    plt.plot(X, Y, 'bo', label='FP Comp')
+    plt.legend()
+
+# 3 - Display plot. 
+
+plt.show()

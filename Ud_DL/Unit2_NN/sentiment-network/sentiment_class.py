b07eb5babcfcb8b883137fb4540ca5358042cebe
david <david.vela.tirado@gmail.com>
2017-08-10 03:12:01 +0200

    sentiment class: mini project 3 - start

---
+++ Ud_DL/Unit2_NN/sentiment-network/sentiment_class.py
@@ -0,0 +1,23 @@
+from collections import Counter
+import numpy as np
+
+g = open('reviews.txt','r') # What we know!
+reviews = list(map(lambda x:x[:-1],g.readlines()))
+g.close()
+
+g = open('labels.txt','r') # What we WANT to know!
+labels = list(map(lambda x:x[:-1].upper(),g.readlines()))
+g.close()
+
+
+
+def pretty_print_review_and_label(i):
+    print(labels[i] + "\t:\t" + reviews[i][:80] + "...")
+
+
+def proof_correlation():
+    # Create three Counter objects to store positive, negative and total counts
+    positive_counts = Counter()
+    negative_counts = Counter()
+    total_counts = Counter()  
+    

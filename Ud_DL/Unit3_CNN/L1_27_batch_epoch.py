01a9106d99557265aea6628ec9fb93bb021d3175
david <david.vela.tirado@gmail.com>
2017-08-13 00:32:28 +0200

    udacity - mini batches

--- Ud_DL/Unit3_CNN/L1_27_batch_epoch.py
+++ Ud_DL/Unit3_CNN/L1_27_batch_epoch.py
@@ -1,5 +1,42 @@
 import tensorflow as tf
+import math
 
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    # TODO: Implement batching
+    output_batches = []
+    sample_size = len(features)
+    for start_i in range(0, sample_size, batch_size):
+        end_i = start_i + batch_size
+        batch = [features[start_i:end_i], labels[start_i:end_i]]
+            
+        output_batches.append(batch)
+    return output_batches
+
+n_input = 128
+n_classes = 10
 features = tf.placeholder(tf.float32, [None, n_input])
 labels = tf.placeholder(tf.float32, [None, n_classes])
 
+# 4 Samples of features
+example_features = [
+    ['F11','F12','F13','F14'],
+    ['F21','F22','F23','F24'],
+    ['F31','F32','F33','F34'],
+    ['F41','F42','F43','F44']]
+# 4 Samples of labels
+example_labels = [
+    ['L11','L12'],
+    ['L21','L22'],
+    ['L31','L32'],
+    ['L41','L42']]
+
+example_batches = batches(3, example_features, example_labels)
+print(example_batches)

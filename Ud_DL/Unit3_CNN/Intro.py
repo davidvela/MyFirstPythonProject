e7d414c2bcedd9431120cbf8fdfac9e01b6670cc
david <david.vela.tirado@gmail.com>
2017-08-12 03:48:16 +0200

    intro to tf in udacity

--- Ud_DL/Unit3_CNN/Intro.py
+++ Ud_DL/Unit3_CNN/Intro.py
@@ -2,8 +2,19 @@ import tensorflow as tf
 
 # Create TensorFlow object called hello_constant
 hello_constant = tf.constant('Hello World!')
-
+a = tf.constant(1, tf.int32)
+b = tf.constant(1, tf.float64)
 with tf.Session() as sess:
     # Run the tf.constant operation in the session
     output = sess.run(hello_constant)
     print(output)
+
+
+# LESSON 13 - LINEAR FUNCTION - LOGISTIC CLASSIFIER 
+# tf.truncated_normal()
+n_features = 120
+n_labels = 5
+weights = tf.Variable(tf.truncated_normal((n_features, n_labels)))
+# tf.zeros()
+n_labels = 5
+bias = tf.Variable(tf.zeros(n_labels))

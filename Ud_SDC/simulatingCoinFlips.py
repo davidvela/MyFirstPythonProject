8301deeaefa428604732d4950cf125589641e248
david <david.vela.tirado@gmail.com>
2017-10-09 00:14:47 +0200

    Ud_sdc

--- Ud_SDC/simulatingCoinFlips.py
+++ Ud_SDC/simulatingCoinFlips.py
@@ -52,3 +52,23 @@ def visualize_one_die(roll_data):
     
 roll_data = simulate_dice_rolls(500)
 visualize_one_die(roll_data)
+
+
+
+#Probabillities of red being red  
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(p)):
+        q[i]=q[i]/s
+    return q
+print sense(p,Z)
